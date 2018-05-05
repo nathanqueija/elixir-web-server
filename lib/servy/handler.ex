36b1c173638d14416bc178a4cc518eb6868e9870
Nathan Queija <nathanqcam@gmail.com>
2018-05-05 20:22:29 -0300

    Update files

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -6,12 +6,14 @@ defmodule Servy.Handler do
 
   alias Servy.Conv
   alias Servy.BearController
+  alias Servy.VideoCam
 
   @pages_path Path.expand("pages", File.cwd!)
 
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
   import Servy.FileHandler, only: [handle_file: 2]
+  import Servy.View, only: [render: 3]
 
   require Logger
   @doc """
@@ -28,6 +30,18 @@ defmodule Servy.Handler do
     # |> emojify
     |> format_response
 
+  end
+
+  def route(%Conv{method: "GET", path: "/sensors"} = conv) do
+    task = Task.async(fn -> Servy.Tracker.get_location("bigfoot") end)
+
+    snapshots =
+      ["cam-1", "cam-2", "cam-3"]
+      |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
+      |> Enum.map(&Task.await/1)
+
+    where_is_bigfoot = Task.await(task)
+    render(conv, "sensors.eex", snapshots: snapshots, location: where_is_bigfoot)
   end
 
   def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
