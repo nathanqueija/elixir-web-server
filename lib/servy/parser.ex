5ea00a7eb8b187c5cd3c4e197ee03f3313b71b99
Nathan Queija <nathanqcam@gmail.com>
2018-05-05 12:22:30 -0300

    Add return key to request and response

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -3,9 +3,9 @@ defmodule Servy.Parser do
   alias Servy.Conv, as: Conv
 
   def parse(request) do
-    [top, params_string] = String.split(request, "\n\n")
+    [top, params_string] = String.split(request, "\r\n\r\n")
 
-    [request_line | header_lines] = String.split(top, "\n")
+    [request_line | header_lines] = String.split(top, "\r\n")
     [method, path, _] = String.split(request_line, " ")
     headers = parse_headers(header_lines, %{})
     params = parse_params(headers["Content-Type"], params_string)
