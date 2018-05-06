f39fcbbba253dcbfb37b88a9cf154904a362d000
Nathan Queija <nathanqcam@gmail.com>
2018-05-05 23:01:12 -0300

    Add pledge controller

---
+++ lib/servy/pledge_controller.ex
@@ -0,0 +1,22 @@
+defmodule Servy.PledgeController do
+  import Servy.View
+
+  def index(conv) do
+    # Gets the recent pledges from the caches
+    pledges = Servy.PledgeServer.recent_pledges()
+    render(conv, "recent_pledges.eex", pledges: pledges)
+  end
+
+  def create(conv, %{"name" => name, "amount" => amount}) do
+    #Sends the pledge to the external service and caches it
+    Servy.PledgeServer.create_pledge(name, String.to_integer(amount))
+
+    %{conv | status: 201, resp_body: "#{name} pledged #{amount}!"}
+  end
+
+  def new(conv) do
+    render(conv, "new_pledge.eex")
+  end
+
+
+end
