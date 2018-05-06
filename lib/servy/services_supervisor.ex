746140f8837093935a2227e8f24e9277b3645c31
Nathan Queija <nathanqcam@gmail.com>
2018-05-06 12:19:09 -0300

    Add services supervisor

---
+++ lib/servy/services_supervisor.ex
@@ -0,0 +1,18 @@
+defmodule Servy.ServicesSupervisor do
+  use Supervisor
+
+  def start_link(_arg) do
+    IO.puts "Starting the services supervisor..."
+    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def init(:ok) do
+    children = [
+      Servy.PledgeServerWithOtpGenServer,
+      Servy.OtpFourOhFourCounter,
+      {Servy.SensorServer, 60}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
