d5eeb266d0e29d4fe6176c2a7f213bdec33e1ac9
Nathan Queija <nathanqcam@gmail.com>
2018-05-06 11:09:16 -0300

    Move asyn code to sensor server

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -7,7 +7,6 @@ defmodule Servy.Handler do
   alias Servy.Conv
   alias Servy.BearController
   alias Servy.PledgeController
-  alias Servy.VideoCam
 
   @pages_path Path.expand("pages", File.cwd!)
 
@@ -34,15 +33,8 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "GET", path: "/sensors"} = conv) do
-    task = Task.async(fn -> Servy.Tracker.get_location("bigfoot") end)
-
-    snapshots =
-      ["cam-1", "cam-2", "cam-3"]
-      |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
-      |> Enum.map(&Task.await/1)
-
-    where_is_bigfoot = Task.await(task)
-    render(conv, "sensors.eex", snapshots: snapshots, location: where_is_bigfoot)
+    sensor_data = Servy.SensorServer.get_sensor_data()
+    render(conv, "sensors.eex", snapshots: sensor_data.snapshots, location: sensor_data.location)
   end
 
   def route(%Conv{method: "POST", path: "/pledges"} = conv) do
